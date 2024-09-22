97d5ace3f2a47ff19d0fb6711f02440e086df8b0
Ege Can Kapar <142752113+Yushiyima@users.noreply.github.com>
2024-09-22 20:05:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def handle_file_upload():
     return send_file(pdf_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
